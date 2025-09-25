0c37919c56de1939a932d6040c9d743571006618
riaz-khan-16 <goriazmte@gmail.com>
2025-09-25 16:26:05 +0600

    initial commit

---
+++ test_calculator.py
@@ -0,0 +1,14 @@
+import calculator
+
+def test_add():
+    assert calculator.add(2, 3) == 5
+
+def test_subtract():
+    assert calculator.subtract(5, 2) == 3
+
+def test_multiply():
+    assert calculator.multiply(3, 4) == 12
+
+def test_divide():
+    assert calculator.divide(10, 2) == 5
+    assert calculator.divide(5, 0) == "Error! Division by zero."
